8dde01654f5edf4d92d43d7bbbf09bc0c78dc185
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-06 03:25:13 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -2,24 +2,24 @@ def call(){
     stages = ["Build&Test", "Sonar", "Run", "Rest", "Nexus"] as String[]
     
         // Si stage es vacio se consideran todos los stages
-    _stage = params.stage ? params.stage.split(';') : stages
+    Stage = params.stage ? params.stage.split(';') : stages
     // Se valida stage ingresado
-    _stage.each { el ->
+    Stage.each { el ->
         if (!stages.contains(el)) {
             throw new Exception("Stage: $el no es una opción válida.")
         }
     }
 
-       if(_stage.contains('Build&Test')) {
+       if(Stage.contains('Build&Test')) {
             stage('Build&Test') {
-              env.LAST_STAGE_NAME = env.STAGE_NAME
+              env.VARIABLE = env.STAGE_NAME
                 sh "./gradlew clean build"
             }
          }   
 
-        if(_stage.contains("Sonar")){
+        if(Stage.contains("Sonar")){
             stage('Sonar'){
-               env.LAST_STAGE_NAME = env.STAGE_NAME
+               env.VARIABLE = env.STAGE_NAME
                def scannerHome = tool 'sonar-scanner';
                withSonarQubeEnv('sonar-server'){ 
                    sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
@@ -27,24 +27,24 @@ def call(){
             }
         }
 
-        if(_stage.contains("Run")){
+        if(Stage.contains("Run")){
             stage('Run'){
-              env.LAST_STAGE_NAME = env.STAGE_NAME
+              env.VARIABLE = env.STAGE_NAME
                 sh 'nohup gradle bootRun &'
                 sleep 10
             }
         }
 
-        if(_stage.contains("Rest")){
+        if(Stage.contains("Rest")){
             stage('Rest'){
-                env.LAST_STAGE_NAME = env.STAGE_NAME
+                env.VARIABLE = env.STAGE_NAME
                 sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
             }
         }
 
-        if(_stage.contains("Nexus")){
+        if(Stage.contains("Nexus")){
             stage('Nexus'){
-                env.LAST_STAGE_NAME = env.STAGE_NAME
+                env.VARIABLE = env.STAGE_NAME
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]    
              }
         }
