ecc059556c775c1fb49c6475e1e00db14d83e1f6
Man <73675618+joramdevops@users.noreply.github.com>
2021-01-07 00:15:57 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -5,7 +5,7 @@ pipeline {
   
     parameters { 
          choice(name: 'buildtool', choices: ['gradle','maven'], description: 'Seleccione la herramienta para la aplicación') 
-         string(name: 'stages', defaultValue: '', description: 'Escriba el stage que quiere ejecutar: "Build&Test", "Sonar", "RunJar", "Rest", "Nexus". Sino escribe se ejecutarán todos')
+      // string(name: 'stages', defaultValue: '', description: 'Escriba el stage que quiere ejecutar: "Build&Test", "Sonar", "RunJar", "Rest", "Nexus". Sino escribe se ejecutarán todos')
     }   
       
     stages {
@@ -17,8 +17,10 @@ pipeline {
                             
                       if(params.buildtool == 'gradle'){ 
                          gradle.call()
+                         string(name: 'stages', defaultValue: '', description: 'Escriba el stage que quiere ejecutar: "Build&Test", "Sonar", "RunJar", "Rest", "Nexus". Sino escribe se ejecutarán todos')
                       }else{
                          maven.call()
+                         string(name: 'stages', defaultValue: '', description: 'Escriba el stage que quiere ejecutar: "Compile", "Test", "Jar", "Sonar", "Run", "Rest", "Nexus". Sino escribe se ejecutarán todos')
                          }
                          }
                       }
